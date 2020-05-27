0ba45f85c196a7f47228c92436074415aff69efd
Grudzien Paul <paul.grudzien@zaclys.net>
2020-05-27 17:56:57 +0200

    add /courses and /courses/:id routes

--- routes/course-routes.js
+++ routes/course-routes.js
@@ -5,12 +5,25 @@ const express = require("express")
 const asyncHandler = require("./asyncHandler.js")
 const authenticateUser = require("./authenticateUser.js")
 
-const {Course} = db.models;
+const {User, Course} = db.models;
 const router = express.Router();
 
-router.get('/courses', async (req, res) => {
+router.get('/courses', asyncHandler(async (req, res) => {
     const courses = await Course.findAll({order: [ ['title', 'ASC'] ]});
+    for(let i=0 ; i<courses.length ; i++) {
+        courses[i].owner = await User.findByPk(courses[i].userId)
+    }
     res.json(courses)
-});
+}));
+
+router.get('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+        owner = await User.findByPk(course.userId);
+        res.json(course);
+    } else {
+        res.status(500).json({ message: 'Server Error' });
+    }
+}));
 
 module.exports = router;
